6e7967c0f2553835c58f0415a728b50b88d00c24
kevine.oyanda <kevine.oyanda@turnkeyafrica.com>
2024-01-22 10:16:07 +0300

    implementing apache kafka

--- inventoryService/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ inventoryService/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.math.BigDecimal;
 
@@ -29,19 +30,10 @@ public class InventoryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
-//    @Bean
-//    public CommandLineRunner loadData(InventoryRepository inventoryRepository){
-//        return args -> {
-//            Inventory inventory = new Inventory();
-//            inventory.setSkuCode("iphone_13");
-//            inventory.setQuantity(BigDecimal.valueOf(100));
-//
-//            Inventory inventory1 = new Inventory();
-//            inventory1.setSkuCode("iphone_14");
-//            inventory1.setQuantity(BigDecimal.valueOf(50));
-//
-//            inventoryRepository.save(inventory);
-//            inventoryRepository.save(inventory1);
-//        };
-//    }
+    @Bean
+    public CommandLineRunner loadData(KafkaTemplate<String, String>  kafkaTemplate){
+        return args -> {
+            kafkaTemplate.send("inventory", "Hello Kevine");
+        };
+    }
 }
---
+++ inventoryService/src/main/java/com/example/inventoryservice/KafkaConfig/KafkaTopic.java
@@ -0,0 +1,14 @@
+package com.example.inventoryservice.KafkaConfig;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopic {
+    @Bean
+    public NewTopic inventoryTopic(){
+        return TopicBuilder.name("inventory").build();
+    }
+}
